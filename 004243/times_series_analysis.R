c215f434274f3a4621980c62f99d230873918b62
J <1292069740@qq.com>
2022-11-22 23:26:06 +0800

    work on ts

--- 004243/times_series_analysis.R
+++ 004243/times_series_analysis.R
@@ -1,8 +1,21 @@
 library(tidyverse)
 library(data.table)
 library(lubridate)
-library(zoo)
-library(xts)
+library(xts) 
 library(dynlm)
 library(vars)
 library(quantmod)
+
+etf <- read_csv("./004243/data/etf.csv")
+oil <- read_csv("./004243/data/oil.csv")
+
+#create xts of etf from 2020-end
+etf_ts <- xts(etf$Close, etf$Date)["2020/"]
+etf_wkly <- to.weekly(etf_ts) #convert to weekly OHLC series
+
+oil_ts <- xts(as.numeric(oil$Close), oil$Date)["2020/"]
+oil_wkly <- to.weekly(oil_ts) #convert to weekly
+
+#combine the weekly etf and oil 
+sum(index(etf_wkly) != index(oil_wkly)) #check time discrepancy
+etf_oil_wkly <- cbind(etf_wkly)
