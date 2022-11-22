3edf211b54d58a25ad39a52b4d0105fbcc0e792d
J <1292069740@qq.com>
2022-11-23 00:22:18 +0800

    plot

--- 004243/times_series_analysis.R
+++ 004243/times_series_analysis.R
@@ -18,4 +18,11 @@ oil_wkly <- to.weekly(oil_ts) #convert to weekly
 
 #combine the weekly etf and oil 
 sum(index(etf_wkly) != index(oil_wkly)) #check time discrepancy
-etf_oil_wkly <- cbind(etf_wkly)
+etf_oil_wkly <- cbind(etf_wkly$etf_ts.Close, oil_wkly$oil_ts.Close)
+sum(is.na(etf_oil_wkly)) #check NAs
+
+plot(log(etf_oil_wkly), plot.type = "single", col = c("blue", "red"),
+     lty = 1:2)
+legend(x = "topleft", legend = c("etf", "oil"), col = c("blue", "red"),
+       lty = 1:2) #?does not work
+cor(etf_oil_wkly$etf_ts.Close, etf_oil_wkly$oil_ts.Close) #showing high correlation
