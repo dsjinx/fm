eb33b64d8a17d4af5c1e6964ca996fc3fbf3f425
J <1292069740@qq.com>
2022-11-23 17:33:22 +0800

    add BIC test to locate AR(p)

--- 004243/times_series_analysis.R
+++ 004243/times_series_analysis.R
@@ -19,10 +19,42 @@ oil_wkly <- to.weekly(oil_ts) #convert to weekly
 #combine the weekly etf and oil 
 sum(index(etf_wkly) != index(oil_wkly)) #check time discrepancy
 etf_oil_wkly <- cbind(etf_wkly$etf_ts.Close, oil_wkly$oil_ts.Close)
+etf_oil_wkly <- log(etf_oil_wkly)
 sum(is.na(etf_oil_wkly)) #check NAs
 
-plot(log(etf_oil_wkly), plot.type = "single", col = c("blue", "red"),
-     lty = 1:2)
-legend(x = "topleft", legend = c("etf", "oil"), col = c("blue", "red"),
-       lty = 1:2) #?does not work
-cor(etf_oil_wkly$etf_ts.Close, etf_oil_wkly$oil_ts.Close) #showing high correlation
+plot.zoo(etf_oil_wkly, plot.type = "single", col = c("blue", "red"),
+         lty = 1:2)
+cor(etf_oil_wkly$etf_ts.Close, etf_oil_wkly$oil_ts.Close) 
+#high correlation between price
+
+plot.zoo(diff(etf_oil_wkly), plot.type = "single", col = c("blue", "red"),
+         lty = 1:2) #graphically cointegration
+cor(diff(log(etf_oil_wkly$etf_ts.Close))[-1], 
+    diff(log(etf_oil_wkly$oil_ts.Close))[-1]) 
+#low correlation between return
+
+#check integration order of each wkly ts
+#BIC method to define the p
+BIC <- function(model) {
+  
+  ssr <- sum(model$residuals^2)
+  t <- length(model$residuals)
+  npar <- length(model$coef)
+  
+  return(
+    round(c("p" = npar - 1,
+            "BIC" = log(ssr/t) + npar * log(t)/t,
+            "R2" = summary(model)$r.squared), 4)
+  )
+}
+
+#search the best p order min BIC
+order <- 1:20
+
+BICs <- sapply(order, function(x) 
+  BIC(dynlm(zoo(etf_oil_wkly$etf_ts.Close) ~ 
+              L(zoo(etf_oil_wkly$etf_ts.Close), 1:x))))
+BICs
+qplot(order, BICs[2,], geom = c("point", "line"))
+p_order <- order[which.min(BICs[2, ])] 
+
